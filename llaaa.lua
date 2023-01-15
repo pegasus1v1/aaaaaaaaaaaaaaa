911b73a5f9102b2adc8fecef5b91c6718c018fea
Joseph <113266325+pegasus1v1@users.noreply.github.com>
2023-01-14 19:01:09 -0500

    Add files via upload

---
+++ llaaa.lua
@@ -0,0 +1,29 @@
+ListaNegra1 = {
+  "Emiliano808097",
+  "MxgicNabil",
+  "BOOGABLOZxD",
+  "BOOGABLOZxDD",
+  "BOOGABLOZxD_FS",
+  "BOOGABLOZxD52",
+  "BOOGABLOZxDD1",
+  "BOOGABLOZxdd12",
+  "BOOGABLOZxD22",
+  "BOOGABLOZxD_Dev",
+  "BOOGABLOZxD_Devv",
+  "BOOGABLOZxDD123",
+  "vegeta_cs12",
+  "terry123456deed",
+}
+
+local ListOfBannedPeople = {
+  "BOOGABLOZxD",
+  "BOOGABLOZxDD",
+  "BOOGABLOZxD_FS",
+  "BOOGABLOZxD52",
+  "BOOGABLOZxDD1",
+  "BOOGABLOZxdd12",
+  "BOOGABLOZxD22",
+  "BOOGABLOZxD_Dev",
+  "BOOGABLOZxD_Devv",
+  "BOOGABLOZxDD123",
+}
